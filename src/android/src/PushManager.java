031d4cf91856e3bd09967991e5090367adb2f259
chenxiao <cx.camel1992@gmail.com>
2018-03-05 15:41:54 +0800

    添加install apk处理

--- src/android/src/PushManager.java
+++ src/android/src/PushManager.java
@@ -87,6 +87,16 @@ public final class PushManager extends CordovaPlugin {
             });
             return true;
         }
+        else if ("installapk".equals(action)) {
+            installApk(args.getString(0));
+        }else if ("toAppDetail".equals(action))//to to app detail
+        {
+            Intent setting = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+            setting.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            setting.setData(Uri.fromParts("package", cordova.getActivity().getPackageName(), null));
+            cordova.getActivity().startActivity(setting);
+            System.exit(0);
+        }
         return super.execute(action, args, callbackContext);
     }
 
@@ -175,6 +185,15 @@ public final class PushManager extends CordovaPlugin {
             PushManager.registerJPush(context);
         }
 
+    }
+    private void installApk(String path) {
+        
+        // 通过Intent安装APK文件
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        i.setDataAndType(Uri.parse(path), "application/vnd.android.package-archive");
+        cordova.getActivity().startActivity(i);
+        android.os.Process.killProcess(android.os.Process.myPid());
     }
 
     public static void registerJPush(Context context) {
